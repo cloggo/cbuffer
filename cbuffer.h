e07d5bf14ade6ca9cc30d16ec22df5e16a0faec3
Aaron Diep <aaron@delite.ca>
2018-09-17 22:51:00 -0600

    initial commit

---
+++ cbuffer.h
@@ -0,0 +1,57 @@
+/**
+ *   \file cbuffer.h
+ *   \brief A Documented file.
+ *
+ *  Detailed description
+ *
+ */
+#ifndef CBUFFER_H
+#define CBUFFER_H
+
+#include <inttypes.h>
+#include "void_ptr_cbuffer.h"
+
+
+// WARNING:
+// * DON'T assume return data was initialized because
+//   returned pointer pointed to data that may have been used previously
+// * data pointed to could be override because the buffer is circular
+
+template <class T>
+class CBuffer  {
+public:
+  CBuffer(uint32_t capacity);
+  ~CBuffer();
+
+  T* incFromRight() {return static_cast<T*>(cbuffer_.incFromRight());}
+  T* decFromRight() {return static_cast<T*>(cbuffer_.decFromRight());}
+  T* decFromLeft() {return static_cast<T*>(cbuffer_.decFromLeft());}
+
+  T* operator++(int) {return static_cast<T*>(cbuffer_.incFromRight());}
+  T* operator--(int) {return static_cast<T*>(cbuffer_.decFromRight());}
+  T* operator--() {return static_cast<T*>(cbuffer_.decFromLeft());}
+
+protected:
+  // WARNING: it does not do deep copy (not follows member pointers)
+  //  void append(const T& v) {cbuffer_.append(&v);}
+  CBuffer(){}
+  CBuffer(const CBuffer& rhs) {}
+  CBuffer& operator=(const CBuffer& rhs) {}
+
+private:
+  T* vbuffer_;
+  VoidPtrCBuffer cbuffer_;
+};
+
+template <class T>
+CBuffer<T>::CBuffer(uint32_t capacity)
+  : vbuffer_(new T[capacity + 1]),
+    cbuffer_(capacity, sizeof(T), vbuffer_) {
+}
+
+template<class T>
+CBuffer<T>::~CBuffer() {
+  delete[] this->vbuffer_;
+}
+
+#endif /* CBUFFER_H */
---
+++ void_ptr_cbuffer.h
@@ -0,0 +1,39 @@
+/**
+ *   \file void_ptr_cbuffer.h
+ *   \brief A Documented file.
+ *
+ *  Detailed description
+ *
+ */
+
+#ifndef VOID_PTR_CBUFFER_H
+#define VOID_PTR_CBUFFER_H
+
+#include <inttypes.h>
+
+class VoidPtrCBuffer {
+public:
+  VoidPtrCBuffer(uint32_t capacity, uint32_t offset, void* buffer);
+  ~VoidPtrCBuffer();
+
+  void* incFromRight();
+  void* decFromRight();
+  void* decFromLeft();
+
+protected:
+  //  void append(const void* v);
+  VoidPtrCBuffer(){}
+  VoidPtrCBuffer(const VoidPtrCBuffer& rhs) {}
+  VoidPtrCBuffer& operator=(const VoidPtrCBuffer& rhs) {}
+
+private:
+  uint32_t capacity_;
+  uint32_t offset_;
+  char* buffer_;
+  char* left_;
+  char* right_;
+
+};
+
+
+#endif /* VOID_PTR_CBUFFER_H */
