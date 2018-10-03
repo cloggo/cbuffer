a8f14fc92b19230c2b8db30051dfb22a1f2fb988
Aaron Diep <aaron@delite.ca>
2018-10-02 18:59:56 -0600

    cbuffer implementation

---
+++ void_ptr_cbuffer.cpp
@@ -0,0 +1,82 @@
+/**
+ *   \file void_ptr_cbuffer.cpp
+ *   \brief A Documented file.
+ *
+ *  Detailed description
+ *
+ */
+
+#include "void_ptr_cbuffer.h"
+#include <cstring>
+
+VoidPtrCBuffer::VoidPtrCBuffer(uint32_t capacity, uint32_t offset, void* buffer)
+  : capacity_(capacity), offset_(offset),
+    buffer_(static_cast<char*>(buffer)),
+    left_(buffer_), right_(buffer_) {
+}
+
+VoidPtrCBuffer::~VoidPtrCBuffer() {
+  //   delete[] this->buffer_;
+}
+
+void* VoidPtrCBuffer::decFromLeft() {
+  if(left_ == right_) {
+    return nullptr;
+  }
+
+  void* tmp = left_;
+
+  if(left_ == (buffer_ + capacity_ * offset_)) {
+    left_ = buffer_;
+  }
+  else {
+    left_ += offset_;
+  }
+
+  return tmp;
+}
+
+void* VoidPtrCBuffer::decFromRight() {
+  if(left_ == right_) {
+    return nullptr;
+  }
+
+  if(right_ == buffer_) {
+    right_ = buffer_ + capacity_ * offset_;
+  }
+  else {
+    right_ -= offset_;
+  }
+
+  return right_;
+}
+
+void* VoidPtrCBuffer::incFromRight() {
+  void* tmp = right_;
+
+  if(right_ == (buffer_ + capacity_ * offset_)) {
+    right_ = buffer_;
+  }
+  else {
+    right_ += offset_;
+  }
+
+  if(left_ == right_) {
+    if(left_ == (buffer_ + capacity_ * offset_)) {
+      left_ = buffer_;
+    }
+    else {
+      left_ += offset_;
+    }
+  }
+
+  return tmp;
+}
+
+#if 0
+
+void VoidPtrCBuffer::append(const void* v) {
+  memcpy(this->incFromRight(), v, offset_);
+}
+
+#endif
